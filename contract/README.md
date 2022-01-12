42e84b6a29c07a2088fb2ffe4de1e0b5b90c97f7
zhoug <90252209+zhoug0x@users.noreply.github.com>
2022-01-12 07:58:58 -0400

    added better boilerplate

--- contract/README.md
+++ contract/README.md
@@ -1,15 +1,7 @@
-# Basic Sample Hardhat Project
+# larva mfers contract
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
+run local blockchain: `npm run chain`
 
-Try running some of the following tasks:
+deploy contract locally: `npm run deploy`
 
-```shell
-npx hardhat accounts
-npx hardhat compile
-npx hardhat clean
-npx hardhat test
-npx hardhat node
-node scripts/sample-script.js
-npx hardhat help
-```
+deploy contract to Rinkeby: `npm run deploy:rinkeby`
--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,13 +1,31 @@
-require("@nomiclabs/hardhat-waffle");
+import * as dotenv from 'dotenv';
+import '@nomiclabs/hardhat-waffle';
+import { task } from 'hardhat/config';
 
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+dotenv.config();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+	const accounts = await hre.ethers.getSigners();
+
+	for (const account of accounts) {
+		console.log(account.address);
+	}
 });
 
-module.exports = {
-  solidity: "0.8.7",
+const hardhatConfig = {
+	solidity: '0.8.10',
+	paths: {
+		artifacts: './artifacts',
+	},
+	networks: {
+		hardhat: {
+			chainId: 1337,
+		},
+		rinkeby: {
+			url: process.env.INFURA_RINKEBY_URL,
+			accounts: [`0x${process.env.PRIVATE_KEY}`],
+		},
+	},
 };
+
+module.exports = hardhatConfig;
